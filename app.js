5034df703546bec66b94d9051be08ca6b235f132
arpityadav <yadavification@gmail.com>
2021-03-05 19:19:11 +0530

    final touches

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const passport = require('passport');
 const express = require('express')
 const app = express();
+const path = require('path')
 const session=require('express-session');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
@@ -29,6 +30,7 @@ const Review=require('./models/reviewSchema')
 app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.json()); 
 app.use(express.urlencoded({ extended:true }))
 
